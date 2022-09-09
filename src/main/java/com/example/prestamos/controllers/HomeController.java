2707b06c1caab7c6f8e5c49070d6788ea6f697d5
ogil <ogil@azteca-comunicaciones.com>
2022-09-08 20:05:29 -0500

    se coloca plantilla

---
+++ src/main/java/com/example/prestamos/controllers/HomeController.java
@@ -0,0 +1,15 @@
+package com.example.prestamos.controllers;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+
+    @GetMapping("inicio")
+    public String inicio(){
+        return "home/inicio";
+    }
+
+}
